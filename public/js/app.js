ec6e0f82b62413da6fd3e37201251def5a2a8a6d
RobertoPinna <pinnapinna@hotmail.it>
2019-07-10 10:55:07 +0200

    local dependencies

--- public/js/app.js
+++ public/js/app.js
@@ -9,6 +9,22 @@ fetch('http://puzzle.mead.io/puzzle').then((response) => {
 })
 
 //fetch('http://localhost:3000/weather?address=boston').then((response) => { on heroku i can just put the link below , remember other changes ( app.js and package.json)
+// heroku in package.json is looking for the start value in scripts and there u have to put the name of the app u want to launch, u can put others value, remember example : dev : nodemon
+// after the changes u can launch the program with : npm run dev 
+// it launch the program seeing the content of dev, so it is like running on terminal with nodemon src/app.js -e js,hbs
+//ofcourse to launch it u have to be in the folder where package.json is placed
+// now we can also not to install some package on npm, we can just put it on dependeces in the package.json file, so we don't have to download things
+// for uninstalling : npm uninstall -g nodemon
+//  so we can install it like local dependences, there we go
+// npm install nodemon@1.2.0 --save-dev
+// so it will be added on package.json like dependences
+// it just need on ur local machine when developing so heroku is not installed on heroku and that's ok , heroku uses just start scripts, so it is lighter, heroku have less work
+// so it is not saved on the production enviroment
+// with global installing the problem is that other people don't know what modules u are using and what versionu are using, if local anyone can know it and work with it
+// still run : npm run dev
+// in fact if i run nodemon from terminal it fails because it is not installed globally, just locally
+
+
 fetch('/weather?address=boston').then((response) => {
     response.json().then((data) => {
         if(data.error)
